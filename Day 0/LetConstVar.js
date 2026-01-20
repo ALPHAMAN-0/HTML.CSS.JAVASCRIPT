b824e1d068e3f022f919c4ba0fb6d5338afcc8ab
SIAM HOSSAIN <huffmancoding.acm@gmail.com>
2026-01-20 14:59:35 +0000

    Day 0

--- Day 0/LetConstVar.js
+++ Day 0/LetConstVar.js
@@ -4,4 +4,16 @@ const accountId = 123;
 let accountEmail = "siam.cse.aiub.learnig";
 var accountPassword = "myPassword123";
 
-accountId = 456;
+// accountId = 456; we can not do this beacuse accountId is constant
+
+// Main differecne between let and var
+var x = 5;
+var x = 10;
+
+let y = 15;
+// let y = 20; we can not do this beacuse y is declared with let
+
+
+console.log(accountId)
+
+console.table([accountEmail, accountPassword]);
