dfce8439844462cd4be18a0bdf87cd876b1f0fc9
SIAM HOSSAIN <huffmancoding.acm@gmail.com>
2026-01-20 14:38:25 +0000

    Day 0

---
+++ Day 0/LetConstVar.js
@@ -0,0 +1,7 @@
+console.log("Hello This is for Learning First to End JavaScript")
+
+const accountId = 123;
+let accountEmail = "siam.cse.aiub.learnig";
+var accountPassword = "myPassword123";
+
+accountId = 456;
